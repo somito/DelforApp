566eff05bc0416827f6f209b078fadae5c4a299f
hu3soan0 <hu3soan0@HU03ON20138>
2016-12-09 15:35:58 +0100

    yeah

---
+++ DelforApp/Delfor.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace DelforApp
+{
+    public class Delfor
+    {
+        public Delfor(string path)
+        {
+            DelforMessageXml = XElement.Load(path);
+            FileName = Path.GetFileName(path);
+        }
+
+        public XElement DelforMessageXml { get; set; }
+        public string FileName { get; set; }
+        public string MessageFrom { get; set; }
+        public string MessageTo { get; set; }
+        public string CustomerPlant { get; set; }
+        public string AliasNumber { get; set; }
+        public string Contract { get; set; }
+        public string LoadingPlace { get; set; }
+        public string ShipToPlace { get; set; }
+        public string Warehouse { get; set; }
+        public string BuyerID { get; set; }
+        public string SellerID { get; set; }
+        public string PlantCode { get; set; }
+        public string Loc1 { get; set; }
+        public string Loc2 { get; set; }
+        public string VWType10 { get; set; }
+
+        XNamespace ns = "http://www.intentia.com/Schemas/mec";
+        XNamespace nsenv = "http://www.intentia.com/MBM_Envelope_1";
+        XNamespace nsxsi = "http://www.w3.org/2001/XMLSchema-instance";
+        XNamespace nsxsd = "http://www.w3.org/2001/XMLSchema";
+
+
+        public string GetMessageFrom(XElement xml)
+        {
+            var from = xml.Descendants()
+              .Where(x => x.Name.LocalName == "from")
+              .Select(x => x.Element(nsenv + "address").Value);
+
+            return string.Join("", from);
+        }
+
+        public string GetMessageTo(XElement xml)
+        {
+            var to = xml.Descendants()
+              .Where(x => x.Name.LocalName == "to")
+              .Select(x => x.Element(nsenv + "address").Value);
+
+            return string.Join("", to);
+        }
+
+        public string GetAliasNumber(XElement xml)
+        {
+            var alias = xml.Descendants()
+              .Where(x => x.Name.LocalName == "LIN")
+              .Descendants()
+              .Where(x => x.Name.LocalName == "cmp01")
+              .Select(x => x.Element(ns + "e01_7140").Value);
+
+            return string.Join(";", alias);
+        }
+
+        public string GetBuyerID(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "NAD")
+                       .Select(x => x.Element(ns + "e01_3035").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "BY")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3039").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+
+        }
+
+        public string GetSellerID(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "NAD")
+                       .Select(x => x.Element(ns + "e01_3035").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "SE")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3039").Value);
+
+                    return string.Join("", ID);
+                }
+
+                else if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "SU")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3039").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+
+        }
+
+        public string GetLoadingPlace(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "NAD")
+                       .Select(x => x.Element(ns + "e01_3035").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "SF")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp03")
+                             .Select(x => x.Element(ns + "e01_3036").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+
+        }
+
+        public string GetShipToPlace(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "NAD")
+                       .Select(x => x.Element(ns + "e01_3035").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "ST")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp03")
+                             .Select(x => x.Element(ns + "e01_3036").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+
+        }
+
+        public string GetPlantCode(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "NAD")
+                       .Select(x => x.Element(ns + "e01_3035").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "ST")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3039").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+        }
+
+        public string GetLoc1(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "LOC")
+                       .Select(x => x.Element(ns + "e01_3227").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "11")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3225").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+        }
+
+        public string GetLoc2(XElement xml)
+        {
+            var type = xml.Descendants()
+                       .Where(x => x.Name.LocalName == "LOC")
+                       .Select(x => x.Element(ns + "e01_3227").DescendantsAndSelf());
+
+            for (int i = 0; i < type.Count(); i++)
+            {
+                if (string.Join("", type.ElementAt(i).ElementAt(0).Value) == "7")
+                {
+                    var ID = type.ElementAt(i).ElementAt(0).ElementsAfterSelf()
+                             .Where(x => x.Name.LocalName == "cmp01")
+                             .Select(x => x.Element(ns + "e01_3225").Value);
+
+                    return string.Join("", ID);
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- DelforApp/Program.cs
+++ DelforApp/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace DelforApp
 {
-    static class Program
+    class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +19,143 @@ namespace DelforApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            string dirpath = "";
+            string filepath = "";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                dirpath = dialog.SelectedPath;
+
+
+                string[] fileEntries = Directory.GetFiles(dirpath);
+
+                //Initialize an excel file and a line number to be passed to the Output method
+                int rowNumber = 1;
+
+                CreateExcel(dirpath);
+
+                bool save = false;
+
+                for (int i = 0; i < fileEntries.Length; i++)
+                {
+                    filepath = fileEntries[i];
+
+                    //Check if the inspected file is a .rcv or .xml or .env, if not, continue to next file
+                    string extension = Path.GetExtension(filepath);
+
+                    if (extension != ".rcv" && extension != ".xml" && extension != ".env")
+                    {
+                        continue;
+                    }
+
+                    Delfor Message = new Delfor(filepath);
+
+                    rowNumber++;
+
+                    //Flag for saving only at last entry to the excel
+                    if (i == fileEntries.Length - 1)
+                    {
+                        save = true;
+                    }
+
+                    OutputToExcel(Message, rowNumber, dirpath + "\\Delfor_Analysis.xlsx", save);
+                }
+
+            }
+        }
+
+        private static void CreateExcel(string dirpath)
+        {
+            //Check if the result excel already exists. If yes, delete it to overwrite
+
+            try
+            {
+                if (File.Exists(dirpath + "\\Delfor_Analysis.xlsx"))
+                {
+                    File.Delete(dirpath + "\\Delfor_Analysis.xlsx");
+                }
+            }
+
+            catch (IOException)
+            {
+                MessageBox.Show("The result excel file exists and is in use.");
+                Environment.Exit(0);
+            }
+
+            //Create the result excel
+
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = false;
+            Directory.SetCurrentDirectory(dirpath);
+            Excel.Workbook wb = excel.Workbooks.Add();
+            Excel.Worksheet sh = wb.Sheets.Add();
+            sh.Name = "Delfor";
+            sh.Cells[1, "A"].Value2 = "FileName";
+            sh.Cells[1, "B"].Value2 = "Envelope:From";
+            sh.Cells[1, "C"].Value2 = "Envelope:To";
+            sh.Cells[1, "D"].Value2 = "Item Alias";
+            sh.Cells[1, "E"].Value2 = "BuyerID";
+            sh.Cells[1, "F"].Value2 = "SellerID";
+            sh.Cells[1, "G"].Value2 = "Loading Place";
+            sh.Cells[1, "H"].Value2 = "Ship-To Place";
+            sh.Cells[1, "I"].Value2 = "Plant Code";
+            sh.Cells[1, "J"].Value2 = "Place of Discharge";
+            sh.Cells[1, "K"].Value2 = "Place of Delivery";
+            sh.Cells[1, "L"].Value2 = "VW Partner Type 10";
+
+            Excel.Range formatRange;
+            formatRange = sh.get_Range("A1");
+            formatRange.EntireRow.Font.Bold = true;
+            formatRange = sh.get_Range("A1, B1, C1, D1, E1, F1, G1, H1, I1, J1, K1, L1");
+            formatRange.EntireColumn.NumberFormat = "@";
+
+            wb.SaveAs(dirpath + "\\Delfor_Analysis.xlsx");
+            wb.Close(true);
+            excel.Quit();
+        }
+
+        private static void OutputToExcel(Delfor Message, int rowNumber, string path, bool save)
+        {
+            Message.MessageFrom = Message.GetMessageFrom(Message.DelforMessageXml);
+            Message.MessageTo = Message.GetMessageTo(Message.DelforMessageXml);
+            Message.AliasNumber = Message.GetAliasNumber(Message.DelforMessageXml);
+            Message.BuyerID = Message.GetBuyerID(Message.DelforMessageXml);
+            Message.SellerID = Message.GetSellerID(Message.DelforMessageXml);
+            Message.LoadingPlace = Message.GetLoadingPlace(Message.DelforMessageXml);
+            Message.ShipToPlace = Message.GetShipToPlace(Message.DelforMessageXml);
+            Message.PlantCode = Message.GetPlantCode(Message.DelforMessageXml);
+            Message.Loc1 = Message.GetLoc1(Message.DelforMessageXml);
+            Message.Loc2 = Message.GetLoc2(Message.DelforMessageXml);
+            Message.VWType10 = Message.SellerID + Message.PlantCode;
+
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = false;
+            Excel.Workbook wb = excel.Workbooks.Open(path);
+            Excel.Worksheet sh = wb.ActiveSheet;
+
+            sh.Cells[rowNumber, "A"] = Message.FileName;
+            sh.Cells[rowNumber, "B"] = Message.MessageFrom;
+            sh.Cells[rowNumber, "C"] = Message.MessageTo;
+            sh.Cells[rowNumber, "D"] = Message.AliasNumber;
+            sh.Cells[rowNumber, "E"] = Message.BuyerID;
+            sh.Cells[rowNumber, "F"] = Message.SellerID;
+            sh.Cells[rowNumber, "G"] = Message.LoadingPlace;
+            sh.Cells[rowNumber, "H"] = Message.ShipToPlace;
+            sh.Cells[rowNumber, "I"] = Message.PlantCode;
+            sh.Cells[rowNumber, "J"] = Message.Loc1;
+            sh.Cells[rowNumber, "K"] = Message.Loc2;
+            sh.Cells[rowNumber, "L"] = Message.VWType10;
+
+            if (save)
+            {
+                wb.Save();
+            }
+
+            wb.Close(true);
+            excel.Quit();
         }
     }
 }
